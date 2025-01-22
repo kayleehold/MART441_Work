b227b24b4d48fbeecc235954a8c65d841dba4743
kayleehold <143434986+kayleehold@users.noreply.github.com>
2025-01-22 09:47:15 -0700

    new hw 2

---
+++ 02_HW/JavaScript.js
@@ -0,0 +1,26 @@
+/* 
+HOMEWORK OUTLINE
+The console.log should include;
+3 of your favorite websites
+3 of your favorite games if you play any - they can be board games, etc.
+3 of your favorite artists and their cultural importance
+Create a simple question and answer that uses prompt and the console.log. You do not need to verify if the answer is correct. */
+
+/* Console Log Three Favorite Websites */
+console.log('https://www.oldbookillustrations.com/');
+console.log('https://www.flickr.com/photos/biodivlibrary/with/53903648865');
+console.log('https://www.pinterest.com/');
+
+/* Console Log 3 Favorite Games */
+console.log('MineCraft');
+console.log('Red Dead Redemption 2');
+console.log('Unpacking');
+
+/* Console Log 3 Artists */
+console.log('Hayao Miyazaki - Japanese animator, filmmaker, and manga artist. He co-founded Studio Ghibli');
+console.log('Chappell Roan - American singer-songwriter known for her upbeat, femme-positive pop music who is rewriting the standards of celebrity culture');
+console.log('');
+
+/* Console Prompt */
+var name = promt('what is your name?');
+console.log('hello' + name);
